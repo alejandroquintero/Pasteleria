ba11ce30dc3eae7ea0c9dc8f28a8ba7376a4827b
alejandroquintero <Asistente@ASISTENTE-139.virtual.uniandes.edu.co>
2017-07-30 22:57:42 -0500

    first commit

---
+++ bakery-api/collections/runners/photoAlbumsCollectionRunner.bat
@@ -0,0 +1,1 @@
+newman run C:\Users\Asistente\Downloads\Pasteleria-HEAD\Pasteleria\bakery-api\collections\postman_collectionPhotoAlbum.json -e C:\Users\Asistente\Downloads\Pasteleria-HEAD\Pasteleria\bakery-api\collections\postman_env.json --disable-unicode
